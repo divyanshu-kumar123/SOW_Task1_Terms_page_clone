88a894d0b8f0dc9f3b91a38decd8f2d02d136170
Divyanshu <divyanshu@gmail.com>
2025-08-15 07:49:04 +0000

    Deploying10

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
-require('dotenv').config();
-const fastify = require('fastify')({ logger: true });
-const sequelize = require('./config/database');
-require('./models/term');
+require("dotenv").config();
+const fastify = require("fastify")({ logger: true });
+const sequelize = require("./config/database");
+require("./models/term");
 
 // Register plugins and routes
-fastify.register(require('@fastify/cors'), { origin: '*' });
-fastify.register(require('./routes/termRoutes'));
+fastify.register(require("@fastify/cors"), { origin: "*" });
+fastify.register(require("./routes/termRoutes"));
 
 // Start server function
 const start = async () => {
   try {
     await sequelize.authenticate();
-    fastify.log.info('Database connection has been established successfully.');
+    fastify.log.info("Database connection has been established successfully.");
 
     const port = process.env.PORT || 3001;
-    await fastify.listen({ port });
+    await fastify.listen({ port, host: "0.0.0.0" });
+    
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
